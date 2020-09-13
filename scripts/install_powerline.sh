64b01ea3495c6cc7dc42ca3c3c07ffd85544be64
RedRuM <44142765+zoj613@users.noreply.github.com>
2020-09-13 16:30:57 +0200

    Use git to install powerline

--- scripts/install_powerline.sh
+++ scripts/install_powerline.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-# First install powerline fonts
-yay -S --needed --noconfirm powerline-fonts
+# Install powerline-fonts from github
+#yay -S --needed --noconfirm powerline-font
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 
-pip3 install powerline-shell
+# Now install powerline-shell
+#pip install powerline-shell
+git clone https://github.com/b-ryan/powerline-shell
+cd powerline-shell
+python setup.py install
+cd ..
+rm -rf powerline-shell
 
