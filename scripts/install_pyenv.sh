aba4b49a8cfe4be8b1be6fe48bf5c4dce3ec4ec8
RedRuM <44142765+zoj613@users.noreply.github.com>
2020-09-13 16:08:16 +0200

    Fix pyenv home variable

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -4,10 +4,10 @@ PYENV_HOST="https://github.com/pyenv"
 PYENV_HOME="${HOME}/.pyenv"
 
 
-git clone ${PYENV_HOST}/pyenv.git PYENV_HOME
+git clone ${PYENV_HOST}/pyenv.git ${PYENV_HOME}
 
 # install pyenv-virtualenv plugin to manage environments
-git clone ${PYENV_HOST}/pyenv-virtualenv.git PYENV_HOME/plugins/pyenv-virtualenv
+git clone ${PYENV_HOST}/pyenv-virtualenv.git ${PYENV_HOME}/plugins/pyenv-virtualenv
 
 # yi all python build system dependencies
 yay -S --needed --noconfirm base-devel openssl zlib xz bzip2 readline sqlite \
