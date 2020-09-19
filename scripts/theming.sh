c1e9ae1c385026374e06c42b733404bff6d064cf
RedRuM <44142765+zoj613@users.noreply.github.com>
2020-09-19 13:25:47 +0200

    fix theming

--- scripts/theming.sh
+++ scripts/theming.sh
@@ -10,30 +10,30 @@ elif [ "XFCE" == "${XDG_CURRENT_DESKTOP}" ]; then
 
     echo "Theming XFCE..."
     # set gtk theme, inspired by https://forum.xfce.org/viewtopic.php?id=9431
-    xfconf-query -c xsettings --create string -p /Net/ThemeName --set "vimix-dark-beryl"
-    xfconf-query -c xsettings --create string -p /Net/IconThemeName --set "maia"
-    xfconf-query -c xsettings --create bool -p /Net/EnableEventSounds --set false
+    xfconf-query -c xsettings --create -t string -p /Net/ThemeName --set "vimix-dark-beryl"
+    xfconf-query -c xsettings --create -t string -p /Net/IconThemeName --set "maia"
+    xfconf-query -c xsettings --create -t bool -p /Net/EnableEventSounds --set false
     # xfconf-query -c xsettings -p /Xft/DPI --set 96
-    xfconf-query -c xsettings --create int -p /Xft/Antialias --set 1
-    xfconf-query -c xsettings --create int -p /Xft/Hinting --set 1
-    xfconf-query -c xsettings --create string -p /Xft/HintingStyle --set "hintslight"
-    xfconf-query -c xsettings --create string -p /Gtk/FontName --set "Droid Sans 10"
+    xfconf-query -c xsettings --create -t int -p /Xft/Antialias --set 1
+    xfconf-query -c xsettings --create -t int -p /Xft/Hinting --set 1
+    xfconf-query -c xsettings --create -t string -p /Xft/HintingStyle --set "hintslight"
+    xfconf-query -c xsettings --create -t string -p /Gtk/FontName --set "Droid Sans 10"
     # xfconf-query -c xsettings -p /Gtk/CursorThemeName --set "xcursor-breeze"
-    xfconf-query -c xsettings --create string -p /Gtk/MonospaceFontName \
+    xfconf-query -c xsettings --create -t string -p /Gtk/MonospaceFontName \
         --set "Inconsolata for Powerline Medium 12"
 
-    xfconf-query -c xfce4-desktop --create string -p /backdrop/screen0/monitorePD1/workspace0/last-image \
+    xfconf-query -c xfce4-desktop --create -t string -p /backdrop/screen0/monitorePD1/workspace0/last-image \
         --set "$(pwd)/img/wallpaper.jpg"
 
-    xfconf-query -c displays --create string -p /Default/ePD1/Resolution --set "1920x1080" || \
-        xfconf-query -c displays --create string -p /Default/ePD1/Resolution --set "1366x768"
+    xfconf-query -c displays --create -t string -p /Default/ePD1/Resolution --set "1920x1080" || \
+        xfconf-query -c displays --create -t string -p /Default/ePD1/Resolution --set "1366x768"
     # move window buttons to the left in the orde: close|minimize|maximize
-    xfconf-query -c xfwm4 -p /general/button_layout --set "CHMO|S"
-    xfconf-query -c xfwm4 -p /general/theme --set "vimix-dark-beryl"
-    xfconf-query -c xfwm4 -p /general/title_font --set "Droid Sans Bold 10"
-    xfconf-query -c xfwm4 -p /general/workspace_count --set 2
+    xfconf-query -c xfwm4 --create -t string -p /general/button_layout --set "CHMO|S"
+    xfconf-query -c xfwm4 --create -t string -p /general/theme --set "vimix-dark-beryl"
+    xfconf-query -c xfwm4 --create -t string -p /general/title_font --set "Droid Sans Bold 10"
+    xfconf-query -c xfwm4 --create -t int -p /general/workspace_count --set 2
 
-    xfconf-query -c xfce4-panel --create bool -p /plugins/plugin-3/show-labels --set false
+    xfconf-query -c xfce4-panel --create -t bool -p /plugins/plugin-3/show-labels --set false
 else
     echo "No theming configured for this Desktop environment."
 fi
