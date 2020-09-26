c5bafe5afc117a6b307daa2c90cde8a8416ccfdc
RedRuM <44142765+zoj613@users.noreply.github.com>
2020-09-26 21:23:04 +0200

    Use latest github source to build and install powerline-go and use gruvbox theme

--- scripts/install_powerline.sh
+++ scripts/install_powerline.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # Setup powerline
-yay -S --needed --noconfirm otf-inconsolata-powerline-git powerline-go-bin
+yay -S --needed --noconfirm otf-inconsolata-powerline-git go
+# set powerline execution path
+export GOPATH="$HOME/.local"
+# get powerline from github and install in "GOPATH" it using go
+go get -u -v github.com/justjanne/powerline-go
 
