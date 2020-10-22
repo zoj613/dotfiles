231be5787af964d712ce68c21208880c41326d84
zo <44142765+zoj613@users.noreply.github.com>
2020-10-23 00:26:01 +0200

    Remove fzf script and source scripts from system install path

--- scripts/configure_fzf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# add fzf keybindings for bash shell
-curl -#o ${HOME}/.fzf.bash https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash
-curl -#o ${HOME}/.fzf.completion.bash https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.bash
-# these  will be sourced inside the bashrc file
