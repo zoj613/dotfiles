2970382dce257f27f82af77818ad5ad0b4d4a7d2
zoj <44142765+zoj613@users.noreply.github.com>
2021-09-20 14:31:28 +0200

    Update vifm plugins to support file preview with ueberzug

--- scripts/configure_file_manager.sh
+++ scripts/configure_file_manager.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 # install all tthe necessary packages needed
-yay -S --needed --noconfirm dragon-drag-and-drop 
+yay -S --needed --noconfirm dragon-drag-and-drop wget ueberzug poppler
 
 # download icons
 wget -P ~/.config/vifm/ https://raw.githubusercontent.com/cirala/vifm_devicons/master/favicons.vifm
 # download colorscheme
  wget -P ~/.config/vifm/colors https://raw.githubusercontent.com/vifm/vifm-colors/master/gruvbox.vifm
+
+ # setup ueberzug for previews of various file formats
+ wget -P ~/.local/bin/ https://raw.githubusercontent.com/cirala/vifmimg/master/vifmimg
+ wget -P ~/.local/bin/ https://raw.githubusercontent.com/cirala/vifmimg/master/vifmrun
+ chmod +x ~/.local/bin/vifmimg ~/.local/bin/vifmrun
 
  # remove .vifm directory if automatically created during installation of vifm
  rm -rf $HOME/.vifm/
