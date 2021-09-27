e18d642864e80a9838501fda2ff91dbb27bcb787
Zolisa Bleki <44142765+zoj613@users.noreply.github.com>
2021-09-27 23:28:10 +0200

    Update install packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -12,7 +12,7 @@ alias yi='yay -S --needed --noconfirm --color=auto'
 # essential packages
 yi git xorg-xrandr curl wget base-devel i3-gaps trash-cli mlocate ufw \
     dmenu dunst xorg-xbacklight lxappearance nitrogen i3exit xterm \
-    xf86-input-synaptics gnupg pass ly
+    xf86-input-synaptics gnupg pass pass-update pam-gnupg ly
 
 # update mlocate database so as to use the locate command
 sudo updatedb
@@ -21,7 +21,7 @@ sudo updatedb
 yi vim ctags fzf fd alacritty vifm
 
 # audio packages
-yi mpd mpc ncmpcpp sox alsa-lib pavucontrol
+yi mpd mpc ncmpcpp sox alsa-lib alsa-utils pavucontrol
 
 # fonts
 yi ttf-inconsolata nerd-fonts-inconsolata ttf-droid noto-fonts
