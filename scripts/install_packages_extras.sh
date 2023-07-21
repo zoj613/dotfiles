894d27c85767f51b924d0430012869d47624565f
Zolisa Bleki <zolisa.bleki@gmail.com>
2023-07-20 22:35:43 -0400

    add cdrip-tools setup via github

--- scripts/install_packages_extras.sh
+++ scripts/install_packages_extras.sh
@@ -12,6 +12,9 @@
 # audio
 yi gstreamer dr14_tmeter mediainfo ffmpeg flac audacious-gtk3 deadbeef # cdrip-tools
 
+# setup cd-tools to use arverify and fixoffset scripts and ensure mandatory packages are installed
+git clone https://github.com/zoj613/cdrip-tools.git ~/dev/
+yi sox ffmpeg libsndfile
 
 # desktop packages
 yi rsync 
